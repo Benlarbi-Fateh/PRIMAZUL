997daaf827dee88b69eb689fb6016a586679f4e2
lamia mezrague <lamiamezrague143@gmail.com>
2025-11-10 20:53:57 +0100

    Remplacement complet du frontend par celui de main

---
+++ frontend/src/context/UserContext.js
@@ -0,0 +1,21 @@
+// ce fichier a été créé pour gérer le contexte
+//  utilisateur dans l'application React,
+// ça veut dire partager les informations de
+// l'utilisateur connecté à travers différents
+//  composants sans avoir à passer les props manuellement à chaque niveau.
+
+import { createContext, useState } from "react";
+
+const UserContext = createContext();
+
+export function UserProvider({ children }) {
+  const [user, setUser] = useState(null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export default UserContext;
